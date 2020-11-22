6e335c586144b97e2db4cd25cdd465fe146c5716
Nabendu <nabendu.biswas@gmail.com>
2020-11-22 14:05:09 +0530

    Part-6 complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import './App.css';
 import EggContainer from './components/EggContainer';
+import HooksEggContainer from './components/HooksEggContainer';
+import HookChickenContainer from './components/HookChickenContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <EggContainer />
+        <HooksEggContainer />
+        <HookChickenContainer />
       </div>
     </Provider>
   );
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,1 +1,2 @@
 export { buyEgg } from './egg/eggActions'
+export { buyChicken } from './chicken/chickenActions'
