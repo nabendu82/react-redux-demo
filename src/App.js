93f6c5d37c166198b6fd8d1606992819ddb18b1b
Nabendu <nabendu.biswas@gmail.com>
2020-12-03 14:33:33 +0530

    Part-8 complete

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import './App.css';
 import EggContainer from './components/EggContainer';
 import HooksEggContainer from './components/HooksEggContainer';
 import HookChickenContainer from './components/HookChickenContainer';
+import NewEggContainer from './components/NewEggContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer egg />
+        <ItemContainer />
         <EggContainer />
         <HooksEggContainer />
         <HookChickenContainer />
+        <NewEggContainer />
       </div>
     </Provider>
   );
