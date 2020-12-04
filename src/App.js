4ce3432fbe010198abc26eb9856745f4c44e8db0
Nabendu <nabendu.biswas@gmail.com>
2020-12-04 14:10:04 +0530

    Part-9 complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import './App.css';
-import EggContainer from './components/EggContainer';
-import HooksEggContainer from './components/HooksEggContainer';
-import HookChickenContainer from './components/HookChickenContainer';
-import NewEggContainer from './components/NewEggContainer';
-import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <ItemContainer egg />
-        <ItemContainer />
-        <EggContainer />
-        <HooksEggContainer />
-        <HookChickenContainer />
-        <NewEggContainer />
+        <UserContainer />
       </div>
     </Provider>
   );
